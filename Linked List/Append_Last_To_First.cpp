b60a56c0e918faa675c4b574cc2f8a9b758e81c1
Aditi Shah <72251182+Aditi1Shah@users.noreply.github.com>
2021-08-02 23:12:59 +0530

    Update Append_Last_To_First.cpp

--- Linked List/Append_Last_To_First.cpp
+++ Linked List/Append_Last_To_First.cpp
@@ -1,3 +1,6 @@
+
+/*You have been given a singly linked list of integers along with an integer 'N'
+Write a function to append the last 'N' nodes towards the front of the singly linked list and returns the new head to the list.*/
 #include<iostream>
 using namespace std;
 class Node{
